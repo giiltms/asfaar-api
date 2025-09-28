29c9b3a8765e1dfea1ac246997174da2e451086e
Muhammad Alhajee <alhajee2009@gmail.com>
2025-09-28 12:42:29 +0100

    fix: module dependency issue

--- src/modules/departments/departments.module.ts
+++ src/modules/departments/departments.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { DepartmentsService } from './departments.service';
 import { DepartmentsController } from './departments.controller';
 import { PrismaService } from '@providers/prisma/prisma.service';
+import { AuthModule } from '@modules/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [DepartmentsController],
   providers: [DepartmentsService, PrismaService],
   exports: [DepartmentsService],
